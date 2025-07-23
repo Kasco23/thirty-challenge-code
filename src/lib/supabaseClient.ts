9f0f3db60e386e24271c4e7cd99a9472fcb6f3c7
Kasco <156666938+Kasco23@users.noreply.github.com>
2025-07-24 00:03:39 +0200

    Fix build error by removing stray backtick in supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
-import {creatcRemixClient} from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 `
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-export const supabase = creatcRemixClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
