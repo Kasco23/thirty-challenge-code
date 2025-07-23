da6b19b48b6fe1c290b75078666ae553731dd881
Kasco <156666938+Kasco23@users.noreply.github.com>
2025-07-23 21:45:08 +0200

    Add App.tsx with basic layout

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className=\"min-h-screen flex items-center justify-center bg-gray-100\">
+      <h1 className=\"text-4xl text-center font-bold\">المنه الاطقة </h1>
+    </div>
+  );
+}
+export default App;
