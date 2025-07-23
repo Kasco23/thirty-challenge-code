c99d48670f81c43eb20ea53fe200028b640891f5
Kasco <156666938+Kasco23@users.noreply.github.com>
2025-07-24 00:07:25 +0200

    Fix unterminated template literal in supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,17 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
-`
+if(supabaseUrl) {
+  console.log('Supabase URL is set');
+} else {
+  console.error('Supabase URL is not set');
+}
+
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
+if(supabaseKey) {
+  console.log('Supabase Key is set');
+} else {
+  console.error('Supabase Key is not set');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
