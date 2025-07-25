871423c5bcaca50cca0922cf5665c513b323a447
Kasco <156666938+Kasco23@users.noreply.github.com>
2025-07-26 01:24:44 +0200

    feat: register SegmentIntro, HostPanel, FinalScores pages in routing

--- thirty-challenge/src/App.tsx
+++ thirty-challenge/src/App.tsx
@@ -5,6 +5,9 @@ import Join from "./pages/Join";
 import QuizRoom from "./pages/QuizRoom";
 import Lobby from "./pages/Lobby";
 import Room from "./pages/Room";
+import SegmentIntro from "./pages/SegmentIntro";
+import HostPanel from "./pages/HostPanel";
+import FinalScores from "./pages/FinalScores";
 
 function App() {
   const location = useLocation();
@@ -12,11 +15,14 @@ function App() {
     <div className="dark min-h-screen bg-gradient-to-tr from-black via-[#10102a] to-accent2">
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Landing />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/room/:roomId" element={<QuizRoom />} />
-          <Route path="/lobby" element={<Lobby />} />
-          <Route path="/room" element={<Room />} />
+          <Route path="/" element={Landing } />
+          <Route path="/join" element={Join} />
+          <Route path="/room/:roomId" element={QuizRoom} />
+          <Route path="/lobby" element={Lobby} />
+          <Route path="/room" element={Room} />
+          <Route path="/segment" element={SegmentIntro} />
+          <Route path="/host" element={HostPanel} />
+          <Route path="/final" element={FinalScores} />
         </Routes>
       </AnimatePresence>
     </div>
