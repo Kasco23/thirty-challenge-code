c2d32420bd517940b37ff4e51e7e92004da8af2f
Kasco <156666938+Kasco23@users.noreply.github.com>
2025-07-25 23:14:48 +0200

    fix: resolve parsing error at line 20 due to invalid character

--- thirty-challenge/src/pages/Join.tsx
+++ thirty-challenge/src/pages/Join.tsx
@@ -17,12 +17,46 @@ export default function Join() {
     navigate(`/room?name=${name}&lflag=${flag}&club=${club}`);
   };
 
-  return (\
-    <div className="min-hscreen flex items-center justify-center">
+  return (\n    <div className="min-hscreen flex justify-center items-center">
       <form
         onSubmit={handleSubmit}
         className="flex flex-col gap-4 items-center bg-glass p-8 rounded-xl"
       >
         <input
           type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           placeholder="هبايلة المان
+        />
+        <select
+          value={ flag }
+          onChange={(e)=> setFlage(e.target.value)}
+          className="px-4 py-2 rounded"
+        >
+          <option value="">-- Select Flag --</option>
+          { flags.map(fl => (
+            <option value={fl} key={fl}>
+                <i className x`fi ifi-flag-${fl}` /> {fl}
+            </option>
+          )}
+        </select>
+        <select
+          value={club }
+          onChange={(e)=> setClube(e.target.value)}
+          className="px-4 py-2 rounded"
+        >
+          <option value="">-- Select Club --</option>
+          { clubs.map((clr) => (
+            <option value={clr} key={clt}>
+                {clur}
+            </option>
+          ))}
+        </select>
+        <button type="submit"
+          className="px-4 ">
+          Start
+        </button>
+      </form>
+    </div>
+  );
+}
