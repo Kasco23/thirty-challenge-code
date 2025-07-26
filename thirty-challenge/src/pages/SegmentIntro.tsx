7e162163726a2c2f3c9d941986dcc6052bae0d35
Kasco <156666938+Kasco23@users.noreply.github.com>
2025-07-26 02:36:05 +0200

    fix: close JSX and correct syntax in SegmentIntro.tsx

--- thirty-challenge/src/pages/SegmentIntro.tsx
+++ thirty-challenge/src/pages/SegmentIntro.tsx
@@ -1,6 +1,5 @@
-import react from 'react';
+import React from 'react';
 
 export default function SegmentIntro() {
-  return (div className="p">Segment Intro Page</div>
-);
+  return (<div className="p">Segment Intro Page</div>);
 }
